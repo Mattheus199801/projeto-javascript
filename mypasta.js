03b3049830caed760e2a1c1e0b4bab7add68ff62
Matheus Nunes <matheus.brisanet@gmail.com>
2025-04-12 19:43:28 -0300

    novo arquivo adicionado 3-

--- mypasta.js
+++ mypasta.js
@@ -1,3 +1,18 @@
 function my5() {
     document.getElementById("demo4").innerHTML = "alterado";
 };
+
+{
+    document.getElementById("demo3").innerHTML = "<h2>Olá, Mundo!</h2>"
+}
+
+{
+    document.getElementById("demo45").innerText ="Vamos lá mano!"
+}
+
+{
+    window.alert(5 + 7);
+}
+{
+    console.log (8 + 5);
+}
